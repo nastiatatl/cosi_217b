c47b7b93b8c31b70ffdb00f98d2c4c57b039ff21
Anastasiia Tatlubaeva <tatlubaevaa@gmail.com>
2024-03-12 23:21:13 -0400

    add database support

--- assignment2/app_flask.py
+++ assignment2/app_flask.py
@@ -4,11 +4,16 @@ To see the pages point your browser at http://127.0.0.1:5000.
 
 """
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 import io
 from ner import SpacyDocument
+from models import db, Entity, create_database_entries
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+# initialize the SQLAlchemy database
+db.init_app(app)
 
 
 # For the website we use the regular Flask functionality and serve up HTML pages.
@@ -18,7 +23,7 @@ def index():
     if request.method == 'GET':
         with io.open('input.txt', 'r', encoding='utf-8') as file:
             input_text = file.read()
-        return render_template('form.html', input=input_text)
+        return render_template('main.html', input=input_text)
     else:
         text = request.form['text']
         doc = SpacyDocument(text)
@@ -33,39 +38,34 @@ def index():
                 markup_paragraphed += line
         
         # dependencies part
-        parsed_sent_list = doc.get_dependencies_by_sentence()
+        parsed_sent_list, all_dependencies = doc.get_dependencies_by_sentence()
+        
+        # create and populate the database
+        database_entries = create_database_entries(doc.get_entities(), all_dependencies)
+        for entry in database_entries:
+            db.session.add(entry)
+        db.session.commit()
+        
         sentences = doc.get_sentences()
         return render_template('result.html', markup=markup_paragraphed, 
                                parsed_sent_list=parsed_sent_list, sentences=sentences)
+        
+@app.route('/database')
+def display_database():
+    entities = Entity.query.all()
+    return render_template('database.html', entities=entities)
 
-# alternative where we use two resources
-
-@app.route('/', methods=['GET'])
-def index_get():
-    with io.open('input.txt', 'r', encoding='utf-8') as file:
-        input_text = file.read()
-    return render_template('form.html', input=input_text)
-
-@app.route('/', methods=['POST'])
-def index_post():
-    text = request.form['text']
-    doc = SpacyDocument(text)
+@app.route('/clear_database', methods=['POST'])
+def clear_database():
+    # delete all entries from the database
+    Entity.query.delete()
+    db.session.commit()
     
-    # NER part
-    markup = doc.get_entities_with_markup()
-    markup_paragraphed = ''
-    for line in markup.split('\n'):
-        if line.strip() == '':
-            markup_paragraphed += '<p/>\n'
-        else:
-            markup_paragraphed += line
-    
-    # dependencies part
-    parsed_sent_list = doc.get_dependencies_by_sentence()
-    sentences = doc.get_sentences()
-    return render_template('result.html', markup=markup_paragraphed, 
-                            parsed_sent_list=parsed_sent_list, sentences=sentences)
+    # redirect back to the main page
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
-
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
